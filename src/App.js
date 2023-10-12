6e087eb95c00b97e74dd400796f8d02ddd11723d
sanisaha <44751151+sanisaha@users.noreply.github.com>
2023-10-13 01:43:28 +0300

    react router and bootstrap set up done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import routes from './routes/routes';
+import { store } from './app/store';
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <Provider store={store}>
+        <RouterProvider router={routes}></RouterProvider>
+      </Provider>
     </div>
   );
 }
---
+++ src/routes/routes.js
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layout/Main";
+import Home from "../pages/Home";
+
+
+
+const routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home />
+            }
+        ]
+    }
+]);
+
+export default routes;
